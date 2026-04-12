92a78c594ed887bf4b5e407d522770223049b939
GG <alekxsandrmartynov6@gmail.com>
2026-04-12 20:27:59 +0300

    Delete Ultra Windows Diagnostic Tool (Safe) directory

--- Ultra Windows Diagnostic Tool (Safe)/Ultra Windows Diagnostic Tool (Safe).cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-where dotnet >nul 2>&1 || (echo Dotnet runtime not found. Install .NET Desktop Runtime 8.0 and try again.& echo.& pause & exit /b 1)
-dotnet "ConsoleApp2.dll"
-if errorlevel 1 (echo.& echo App exited with error %%errorlevel%%.& pause)
-
